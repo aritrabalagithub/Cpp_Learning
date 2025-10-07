d0e0f7552b4c0cf617718391c6c87082a1767fdd
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-10-07 07:50:17 +0000

    updated

--- first.cpp
+++ first.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hello world";
+   // cout<<"hello world";
+    int n,f=1;
+   cin>>n;
+   if (n<0) return -11;
+    for(int i=2;i<=n;i++)f*=i;
+    cout<<"factorial: "<<f;
     return 0;
 }
