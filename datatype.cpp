4adaef7a721eb31303f39883fcc9101615c7b4df
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-10-07 18:35:35 +0000

    operators update

---
+++ datatype.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<sizeof(double)<<" bytes";
+    // string str;
+    // cin>>str;
+    // cout<<str;
+return 0;
+}
--- first.cpp
+++ first.cpp
@@ -4,7 +4,10 @@ int main(){
    // cout<<"hello world";
     int n,f=1;
    cin>>n;
-   if (n<0) return -11;
+   if (n<0) {
+    cout<<"factorial not defined for negative integer";
+    return 0;
+   }
     for(int i=2;i<=n;i++)f*=i;
     cout<<"factorial: "<<f;
     return 0;
